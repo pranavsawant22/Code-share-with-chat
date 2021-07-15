80c513fc39995fb12b9305ef20b5d18f1f276d8b
pranavsawant22 <doomboy1212@gmail.com>
2021-07-15 20:52:56 +0530

    first

--- public/chat.js
+++ public/chat.js
@@ -33,3 +33,52 @@ socket.on('typing', function(data){
 
 
 //for background animation
+
+
+const urlParams = new URLSearchParams(location.search);
+
+let roomId = urlParams.get("id");
+
+if (!roomId) {
+  roomId = Math.floor(Math.random() * 10000 + 10000);
+  window.location.search = `id=${roomId}`;
+}
+
+const textArea = document.querySelector("textarea");
+
+const worker = new Worker("worker.js");
+
+const wsurl = "wss://demo.piesocket.com/v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self"
+
+const socket1 = new WebSocket(wsurl);
+
+const debounce = (func, timer = 100) => {
+  let timeId = null;
+  return (...args) => {
+    if (timeId) {
+      clearTimeout(timeId);
+    }
+    timeId = setTimeout(() => {
+      func(...args);
+    }, timer);
+  };
+};
+
+socket1.onopen = () => {};
+
+// const ae = new Audio("https://www.google.com/logos/fnbx/animal_sounds/cat.mp3");
+socket1.onmessage = (e) => {
+  //   console.log(e.data);
+//   ae.play();
+  textArea.value = e.data;
+};
+
+textArea.addEventListener(
+  "input",
+  debounce((e) => {
+    console.log(e.target.value);
+    socket1.send(e.target.value);
+  })
+);
+
+//
